d73444212e80b076213acb13d31298f739b615b6
StarCpt <86216339+StarCpt@users.noreply.github.com>
2025-03-16 11:50:26 -0700

    Add wpf support for Rgb format with binary transparency (tRNS)

--- SharpPng.Wpf/PngSharpWpfExtensions.cs
+++ SharpPng.Wpf/PngSharpWpfExtensions.cs
@@ -62,6 +62,7 @@ namespace SharpPng.Wpf
                             format = PixelFormats.Rgba64;
                             byte[] newImageBytes = new byte[imageBytes.Length * 4];
 
+                            // Work with 16-bit elements since it's a bit more readable
                             Span<ushort> imageSpan16 = MemoryMarshal.Cast<byte, ushort>(imageBytes);
                             Span<ushort> newImageSpan16 = MemoryMarshal.Cast<byte, ushort>(newImageBytes);
 
@@ -93,23 +94,51 @@ namespace SharpPng.Wpf
             }
             else if (info.Format is PngPixelFormat.Rgb)
             {
-                if (info.Transparency?.TransparentColorRgb is (ushort R, ushort G, ushort B) alphaColor)
+                if (info.Transparency?.TransparentColorRgb is (ushort alphaR, ushort alphaG, ushort alphaB) alphaColor)
                 {
-                    throw new NotImplementedException();
-                    byte[] newImageBytes;
                     switch (info.BitDepth)
                     {
                         case 8:
                             format = PixelFormats.Bgra32;
-                            newImageBytes = new byte[info.Width * info.Height * PixelFormats.Bgra32.BitsPerPixel / 8];
-                            for (int i = 0; i < imageBytes.Length; i += 3)
+                            byte[] newImageBytes = new byte[info.Width * info.Height * format.BitsPerPixel / 8];
+                            byte alphaR8 = (byte)(alphaR & 0xff);
+                            byte alphaG8 = (byte)(alphaG & 0xff);
+                            byte alphaB8 = (byte)(alphaB & 0xff);
+                            for (int i = 0, j = 0; i < imageBytes.Length; i += 3, j += 4)
                             {
+                                byte r = imageBytes[i + 0];
+                                byte g = imageBytes[i + 1];
+                                byte b = imageBytes[i + 2];
+                                byte alpha = (r == alphaR8 && g == alphaG8 && b == alphaB8) ? byte.MinValue : byte.MaxValue;
 
+                                newImageBytes[j + 0] = b;
+                                newImageBytes[j + 1] = g;
+                                newImageBytes[j + 2] = r;
+                                newImageBytes[j + 3] = alpha;
                             }
+                            imageBytes = newImageBytes;
                             break;
                         case 16:
                             format = PixelFormats.Rgba64;
-                            newImageBytes = new byte[info.Width * info.Height * PixelFormats.Rgba64.BitsPerPixel / 8];
+                            newImageBytes = new byte[info.Width * info.Height * format.BitsPerPixel / 8];
+
+                            // Work with 16-bit elements since it's a bit more readable
+                            Span<ushort> imageSpan16 = MemoryMarshal.Cast<byte, ushort>(imageBytes);
+                            Span<ushort> newImageSpan16 = MemoryMarshal.Cast<byte, ushort>(newImageBytes);
+
+                            for (int i = 0, j = 0; i < imageSpan16.Length; i += 3, j += 4)
+                            {
+                                ushort r = imageSpan16[i + 0];
+                                ushort g = imageSpan16[i + 1];
+                                ushort b = imageSpan16[i + 2];
+                                ushort alpha = (r == alphaR && g == alphaG && b == alphaB) ? ushort.MinValue : ushort.MaxValue;
+
+                                newImageSpan16[j + 0] = r;
+                                newImageSpan16[j + 1] = g;
+                                newImageSpan16[j + 2] = b;
+                                newImageSpan16[j + 3] = alpha;
+                            }
+                            imageBytes = newImageBytes;
                             break;
                         default: throw new Exception($"Invalid bit depth for {info.Format} format. BitDepth={info.BitDepth}");
                     };
@@ -168,6 +197,7 @@ namespace SharpPng.Wpf
                         format = PixelFormats.Rgba64;
                         newImageBytes = new byte[info.Width * info.Height * format.BitsPerPixel / 8];
 
+                        // Work with 16-bit elements since it's a bit more readable
                         Span<ushort> imageSpan16 = MemoryMarshal.Cast<byte, ushort>(imageBytes);
                         Span<ushort> newImageSpan16 = MemoryMarshal.Cast<byte, ushort>(newImageBytes);
 
@@ -191,6 +221,7 @@ namespace SharpPng.Wpf
                 switch (info.BitDepth)
                 {
                     case 8:
+                        // There is no Rgba32 PixelFormat in wpf so the pixels need to be converted to Bgra32
                         format = PixelFormats.Bgra32;
                         for (int i = 0; i < imageBytes.Length; i += 4)
                         {
